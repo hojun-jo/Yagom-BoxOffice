271a959a85af1752c79ec6d4906c3f3eed07c6c4
조호준 <hojun@johojun-ui-MacBookAir.local>
2023-12-10 20:57:45 +0900

    feat: JustURL 생성

---
+++ BoxOffice/Model/Network/API/JustURL.swift
@@ -0,0 +1,25 @@
+//
+//  JustURL.swift
+//  BoxOffice
+//
+//  Created by 조호준 on 2023/12/10.
+//
+
+import Foundation
+
+struct JustURL: APIType {
+    let baseURL: String
+    var headers: [String : String]? {
+        return nil
+    }
+    var queryItems: [URLQueryItem]? {
+        return nil
+    }
+    var apiKey: String? {
+        return nil
+    }
+    
+    init(url: String) {
+        baseURL = url
+    }
+}
