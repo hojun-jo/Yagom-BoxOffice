c61a9698a47f188f602ed94f30003b31894dd8c0
조호준 <hojun@johojun-ui-MacBookAir.local>
2023-12-10 20:51:01 +0900

    feat: BoxOfficeAPI 생성

---
+++ BoxOffice/Model/Network/API/BoxOffice/BoxOfficeAPI.swift
@@ -0,0 +1,28 @@
+//
+//  BoxOfficeAPI.swift
+//  BoxOffice
+//
+//  Created by 조호준 on 2023/12/10.
+//
+
+import Foundation
+
+struct BoxOfficeAPI: APIType {
+    var baseURL: String {
+        return "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
+    }
+    var headers: [String : String]? {
+        return nil
+    }
+    var queryItems: [URLQueryItem]?
+    var apiKey: String? {
+        return Bundle.main.kobisAPIKey
+    }
+    
+    init(boxOfficeDate: String) {
+        queryItems = [
+            URLQueryItem(name: "key", value: apiKey),
+            URLQueryItem(name: "targetDt", value: boxOfficeDate)
+        ]
+    }
+}
