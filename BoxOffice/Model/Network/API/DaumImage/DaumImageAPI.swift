10f1b216ed7a3000de227e6768e774de209a5789
조호준 <hojun@johojun-ui-MacBookAir.local>
2023-12-10 20:55:58 +0900

    feat: DaumImageAPI 생성

---
+++ BoxOffice/Model/Network/API/DaumImage/DaumImageAPI.swift
@@ -0,0 +1,32 @@
+//
+//  DaumImageAPI.swift
+//  BoxOffice
+//
+//  Created by 조호준 on 2023/12/10.
+//
+
+import Foundation
+
+struct DaumImageAPI: APIType {
+    var baseURL: String {
+        return "https://dapi.kakao.com/v2/search/image"
+    }
+    var headers: [String : String]?
+    var queryItems: [URLQueryItem]?
+    var apiKey: String? {
+        return Bundle.main.kakaoAPIKey
+    }
+    
+    init?(movieName: String) {
+        guard let apiKey else {
+            return nil
+        }
+        
+        queryItems = [
+            URLQueryItem(name: "query", value: "\(movieName)+영화+포스터")
+        ]
+        headers = [
+            "Authorization" : apiKey
+        ]
+    }
+}
